8de650639ace77efaa9a9261d28c874c06ce9d3d
Will Richman <wrichman@gmail.com>
2017-06-13 10:09:37 -0700

    Initial Commit

---
+++ HelloNBC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloNBC
+//
+//  Created by William Richman on 6/13/17.
+//  Copyright © 2017 wrichman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
